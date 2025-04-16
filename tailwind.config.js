dbf5ff2eb97405e9f8b19476991a7b2454beda54
indukorlepara <79860338+indukorlepara@users.noreply.github.com>
2025-04-16 15:19:42 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            colors: {
+                'primary': '#1D4ED8',  // Custom primary color
+                'secondary': '#9333EA', // Custom secondary color
+            },
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [forms, typography],
+};
